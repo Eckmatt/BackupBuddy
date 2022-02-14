05f84195db752e88fa5d6542ba13bb50c46a11d4
ChapstickDealer <meckhardt14@yahoo.com>
2022-02-13 20:34:03 -0500

    Update credentialtest.ps1

--- ps/credentialtest.ps1
+++ ps/credentialtest.ps1
@@ -1,7 +1,9 @@
-
+$testpath = "c:\Users\me\path\To\stored-creds"
 # This method of storing credentials is specific to the machine and the user.
-$credential = Get-Credential
-$credential | Export-CliXml -Path 'cred.xml'
+if (Test-Path -Path $testpath -PathType Leaf){
 
-
-$credential = Import-CliXml -Path 'cred.xml'
+  $credential = Get-Credential
+  $credential | Export-CliXml -Path 'cred.xml'
+}else{
+  $credential = Import-CliXml -Path 'cred.xml'
+}
