0d58695e8801648f579d104609c658273f7d692a
ChapstickDealer <meckhardt14@yahoo.com>
2022-02-20 16:34:45 -0500

    2/20/2022 edits

--- ps/credentialtest.ps1
+++ ps/credentialtest.ps1
@@ -1,24 +1,23 @@
 # a series of functions designed to grab and deploy credentials
-function credential-manager() {
+function get-scissors() {
 # By default, the path to the credential file is stored here. The function is designed such that a user will be able to tell the program where to look for
 # or where to store the credential file.
-  $testpath = "path\To\stored-creds"
-  if (!$args[0]) {
-    continue
-  }else{
-    $testpath = $args[0]
-  }
+  Write-Host "This is actually getting run"
+  $testpath = "bigpairofscissors/scissor1.xml"
 
 
 
 # This method of storing credentials is specific to the machine and the user.
   if (-not (Test-Path -Path $testpath -PathType Leaf)){
-    Write-Host "Credential file not found - creating new credentials..."
+    Write-Host "!!! Credential file not found - creating new credentials... !!!"
     $credential = Get-Credential
     $credential | Export-CliXml -Path $testpath
   }else{
-    Write-Host "Retrieving Credentials
+    Write-Host "!!! Retrieving Credentials !!!"
     $credential = Import-CliXml -Path $testpath
   }
   return $credential
 }
+
+$returncreds = get-scissors
+Write-Host $returncreds
