75b9368444197671a5ee551df7f4ecc531848f11
ChapstickDealer <meckhardt14@yahoo.com>
2022-04-11 01:22:42 -0400

    Update vmentry.ps1

--- ps/vmentry.ps1
+++ ps/vmentry.ps1
@@ -1,12 +1,17 @@
 ﻿#Allows the user to enter VM names for backup
 $looper=1
+$BigObject = Get-Content "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json" | ConvertFrom-Json
+$targets = $BigObject.targets
+
 
 while($looper -eq 1){
-    $entry = Read-Host -Prompt "Please enter the Name of the Virtual Machine you wish to back up, or leave entry blank to exit."
-    if($entry){
-        $entry >> VMlist.txt
+    $entry = Read-Host -Prompt "Please enter the Full Name of the Virtual Machine you wish to back up, or leave entry blank to exit."
+    if($entry -and $targets.Contains($entry)){ 
+        $targets += $entry
+    }elif($entry -and !($targets.Contains($entry)){
+        Read-Host
     }else{
         $looper=0
-    }
-
+    }fi
 }
+$targets 
