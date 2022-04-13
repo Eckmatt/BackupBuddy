7a671b1c744396aa723248d6c042bee277738c39
ChapstickDealer <meckhardt14@yahoo.com>
2022-04-13 19:21:09 -0400

    solved data entry

--- ps/directoryentry.ps1
+++ ps/directoryentry.ps1
@@ -1,12 +1,22 @@
-﻿#Allows the user to enter directory names for backup
+﻿#Allows the user to enter Directory names for backup
 $looper=1
+$BigObject = Get-Content "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json" | ConvertFrom-Json
+$targets = $BigObject.dirTargets
+
 
 while($looper -eq 1){
-    $entry = Read-Host -Prompt "Please enter the full filepath of the directory you wish to back up, or leave entry blank to exit."
-    if($entry){
-        $entry >> directorylist.txt
+    $entry = Read-Host -Prompt "Please enter the full file path of the directory you wish to back up, or leave entry blank to exit."
+    if($entry -and $targets.Contains($entry)){ 
+        Write-Output "Named Directory already exists as a target."
+    }elseif($entry -and !($targets.Contains($entry))){
+        $entry -replace "\$", "\\"
+        Write-Output $entry
+        $targets += $entry
+
+
     }else{
         $looper=0
     }
-
 }
+$BigObject.dirTargets = $targets
+$BigObject | Convertto-Json -Depth 100 | Out-File "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json"
--- ps/vmentry.ps1
+++ ps/vmentry.ps1
@@ -1,17 +1,18 @@
 ﻿#Allows the user to enter VM names for backup
 $looper=1
 $BigObject = Get-Content "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json" | ConvertFrom-Json
-$targets = $BigObject.targets
+$targets = $BigObject.vmTargets
 
 
 while($looper -eq 1){
     $entry = Read-Host -Prompt "Please enter the Full Name of the Virtual Machine you wish to back up, or leave entry blank to exit."
     if($entry -and $targets.Contains($entry)){ 
+        Write-Output "Named VM already exists as a target."
+    }elseif($entry -and !($targets.Contains($entry))){
         $targets += $entry
-    }elif($entry -and !($targets.Contains($entry)){
-        Read-Host
     }else{
         $looper=0
-    }fi
+    }
 }
-$targets 
+$BigObject.vmTargets = $targets
+$BigObject | Convertto-Json -Depth 100 | Out-File "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json"
