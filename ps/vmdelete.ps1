516e0cb45ec7f40312734e0a7feedb78598de929
ChapstickDealer <meckhardt14@yahoo.com>
2022-04-19 18:42:35 -0400

    FREE The DELETE

---
+++ ps/vmdelete.ps1
@@ -0,0 +1,20 @@
+#Allows the user to delete VM names from backup
+$looper=1
+$BigObject = Get-Content "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json" | ConvertFrom-Json
+$targets = $BigObject.vmTargets
+
+while($looper -eq 1){
+    $entry = Read-Host -Prompt "Please enter the Name of the Virtual Machine you wish to remove from Inventory, or leave entry blank to exit."
+    if($entry -and $targets.Contains($entry)){ 
+        $choice = Read-Host -Prompt "Are you sure you wish to delete this VM from Inventory?[y/N]"
+		if($choice = "y" || $choice = "Y")
+		$targets.Remove($entry)
+    }#elseif($entry -and !($targets.Contains($entry))){
+       # $targets += $entry
+    }#else{
+       # $looper=0
+    }
+	
+}
+$BigObject.vmTargets = $targets
+$BigObject | Convertto-Json -Depth 100 | Out-File "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json"
