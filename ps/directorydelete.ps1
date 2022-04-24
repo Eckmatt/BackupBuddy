8b5206cd7d86aca63abad463673f41b2cb05ec5a
ChapstickDealer <meckhardt14@yahoo.com>
2022-04-24 19:43:55 -0400

    Inventory Complete

--- ps/directorydelete.ps1
+++ ps/directorydelete.ps1
@@ -5,12 +5,12 @@ $targets = $BigObject.dirTargets
 Write-Output $targets
 
 while($looper -eq 1){
-    $entry = Read-Host -Prompt "Please enter the Name of the Virtual Machine you wish to remove from Inventory, or leave entry blank to exit."
+    $entry = Read-Host -Prompt "Please enter the Name of the Directory you wish to remove from Inventory, or leave entry blank to exit"
 
 
     if($entry -and $targets.Contains($entry)){ 
 
-        $choice = Read-Host -Prompt "Are you sure you wish to delete this VM from Inventory?[y/N]"
+        $choice = Read-Host -Prompt "Are you sure you wish to delete this Directory from Inventory?[y/N]"
 		if(($choice -eq "y") -or ($choice -eq "Y")){
 		    $targets = $targets | Where-Object {$_ -ne $entry}
             Write-Output "Entry Deleted."
@@ -19,6 +19,10 @@ while($looper -eq 1){
         }else{
             Write-Output "Bad Input, Please enter 'y/Y' or 'n/N'."
         }
+
+    }elseif($entry -and ($targets -eq $null)){
+        Write-Output "ERROR, Directory List is Empty - Please use 'Add Directories' to set directories up for backup."
+        $looper=1
 
     }elseif($entry -and !($targets.Contains($entry))){
         Write-Output "Entered name does not exist in the entry."
--- ps/vmdelete.ps1
+++ ps/vmdelete.ps1
@@ -5,19 +5,26 @@ $targets = $BigObject.vmTargets
 
 while($looper -eq 1){
     $entry = Read-Host -Prompt "Please enter the Name of the Virtual Machine you wish to remove from Inventory, or leave entry blank to exit."
+
     if($entry -and $targets.Contains($entry)){ 
 
         $choice = Read-Host -Prompt "Are you sure you wish to delete this VM from Inventory?[y/N]"
 		if(($choice -eq "y") -or ($choice -eq "Y")){
-		    $targets.Remove($entry)
+		    $targets = $targets | Where-Object {$_ -ne $entry}
             Write-Output "Entry Deleted."
         }elseif(($choice -eq "n") -or ($choice -eq "N")){
             Write-Output "Cancelled"
         }else{
             Write-Output "Bad Input, Please enter 'y/Y' or 'n/N'."
         }
+
+    }elseif($entry -and ($targets -eq $null)){
+        Write-Output "ERROR, VM List is Empty - Please use 'Add VMs' to set VMs up for backup."
+        $looper=1
+
     }elseif($entry -and !($targets.Contains($entry))){
         Write-Output "Entered name does not exist in the entry."
+
     }else{
         $looper=0
     }
