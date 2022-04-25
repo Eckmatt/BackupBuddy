e4f15c74b9dd1d6f6c7a0ef407ebf37be677e92d
ChapstickDealer <meckhardt14@yahoo.com>
2022-04-24 23:30:58 -0400

    Sprint 4

--- ps/directoryutils.ps1
+++ ps/directoryutils.ps1
@@ -1,2 +1,10 @@
 ﻿# Directory Utils
 
+$BigObject = Get-Content "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json" | ConvertFrom-Json
+$targets = $BigObject.dirTargets
+$rocksFolder = $BigObject.rocksFolder
+
+
+foreach($target in $targets){
+    Compress-Archive -Path $target -DestinationPath $rocksFolder
+}
