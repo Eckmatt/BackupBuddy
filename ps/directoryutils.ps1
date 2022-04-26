1fab7385be8b9c3c30529318c42e39d53ff7318f
Eckmatt <meckhardt14@yahoo.com>
2022-04-25 20:59:56 -0400

    finished directory utils

--- ps/directoryutils.ps1
+++ ps/directoryutils.ps1
@@ -1,10 +1,14 @@
 ﻿# Directory Utils
 
-$BigObject = Get-Content "C:\Users\meckh\Documents\GitHub\ChamplainCapstone\ps\config.json" | ConvertFrom-Json
+$BigObject = Get-Content "C:\Users\meckh\OneDrive\Documents\GitHub\ChamplainCapstone\ps\config.json" | ConvertFrom-Json
 $targets = $BigObject.dirTargets
 $rocksFolder = $BigObject.rocksFolder
+$rocksFolder.Trim('"')
 
 
 foreach($target in $targets){
-    Compress-Archive -Path $target -DestinationPath $rocksFolder
+    $archiveName=$target.Replace("\","_")
+    $archiveName+=".zip"
+    $target+="\*"
+    & "C:\Program Files\7-Zip\7z.exe" a -tzip $archiveName -o$rocksFolder $target
 }
